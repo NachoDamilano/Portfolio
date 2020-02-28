734ab02d2ca140f9cd7463969fdb434b3037be85
Javier Vidal <javier_07_95@hotmail.com>
2020-02-28 00:12:01 -0300

    Agrego estructuracion general

---
+++ new/startbootstrap-grayscale-gh-pages/js/nacho.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+function animateCSS(element, animationName, callback) {
+    var node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
+
+  $(".text-hate").click(function(){
+    var element =  document.querySelector('.div-hate')
+    element.classList.add('animated','fadeOut')
+    element.addEventListener('animationend', function(){ 
+     
+     $(".new-text").removeClass("sr-only")
+     animateCSS(".new-text","fadeIn")
+     })
+
+  });
+
+
+});
